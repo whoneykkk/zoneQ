1ba2be4c337229b148e5df100df2f0b4ed13e16d
whoenykkk <whoneykim@gmail.com>
2026-04-19 00:19:42 +0900

    feat: add design tokens and reset styles

---
+++ frontend/src/utils/colors.js
@@ -0,0 +1,27 @@
+export const ZQ = {
+  bg: '#F5F5F3',
+  card: '#FFFFFF',
+  text: '#2C2C2B',
+  text2: '#1A1C1B',
+  textSec: '#414752',
+  textMute: '#A19E99',
+  border: '#E9E9E7',
+  blue: '#2783DE',
+  black: '#1A1C1B',
+  S: '#2D7A4F',  Sbg: '#E8F1EC',
+  A: '#1A6DB5',  Abg: '#EAF3FF',
+  B: '#9B3A6E',  Bbg: '#FAE9F1',
+  C: '#B83228',  Cbg: '#FCE9E9',
+  avail: '#F1F0EF', availText: '#A19E99',
+  chartBar: '#005DA7',
+  shadow: '0px 1px 2px rgba(0,0,0,0.05)',
+  SoccBg: '#E8F1EC',               SoccText: 'rgba(45,122,79,0.6)',
+  AoccBg: 'rgba(26,109,181,0.4)',  AoccText: '#1A6DB5',
+  BoccBg: 'rgba(155,58,110,0.4)', BoccText: '#9B3A6E',
+  CoccBg: 'rgba(184,50,40,0.4)',  CoccText: '#B83228',
+}
+
+export const ZONE_COLOR = { S: ZQ.S, A: ZQ.A, B: ZQ.B, C: ZQ.C }
+export const ZONE_BG    = { S: ZQ.Sbg, A: ZQ.Abg, B: ZQ.Bbg, C: ZQ.Cbg }
+export const OCC_BG     = { S: ZQ.SoccBg, A: ZQ.AoccBg, B: ZQ.BoccBg, C: ZQ.CoccBg }
+export const OCC_TEXT   = { S: ZQ.SoccText, A: ZQ.AoccText, B: ZQ.BoccText, C: ZQ.CoccText }
---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,3 @@
+export const GRADE_THRESHOLDS = { S: 40, A: 50, B: 60 }
+export const NOISE_INTERVAL_MS = 30_000
+export const ZONES = ['S', 'A', 'B', 'C']
